38c3606a0152b07e28c168bc44807c66be972032
Tyson <tyson@horsewell.me>
2023-12-01 08:28:29 +0800

    PyBank analyse coding

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 # Modules
 import os
 import csv
+import copy
 
 # Set path for file
 csvpath = os.path.join(".", "Resources", "budget_data.csv")
@@ -15,6 +16,13 @@ greatest_inc_amount = 0
 greatest_dec_date   = ""
 greatest_dec_amount = 0
 
+r_amount = 0
+p_amount = 0
+r_date = ""
+r_date = ""
+
+
+
 # Open the CSV using the UTF-8 encoding
 with open(csvpath, encoding='UTF-8') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
@@ -23,7 +31,24 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     header_row = next(csv_reader)   
 
     for row in csv_reader:
-        data = row
+        r_date   = copy.deepcopy(row[0])
+        r_amount = int(copy.deepcopy(row[1]))
+
+        total_months += 1
+        total_amount += int(row[1])
+
+        if greatest_inc_amount < (r_amount - p_amount):
+            greatest_inc_date   = r_date
+            greatest_inc_amount = (r_amount - p_amount)
+
+        if greatest_dec_amount > (r_amount - p_amount):
+            greatest_dec_date   = r_date
+            greatest_dec_amount = (r_amount - p_amount)
+
+        p_amount = r_amount
+        p_date   = r_date
+
+        
 
 # analysis
 
@@ -33,13 +58,13 @@ with open(csvpath, encoding='UTF-8') as csvfile:
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in profits (date and amount) over the entire period
 
-total_months        = 86
-total_amount        = 22564198
+#total_months        = 86
+#total_amount        = 22564198
 average_change      = -8311.11
-greatest_inc_date   = "Aug-16"
-greatest_inc_amount = 1862002
-greatest_dec_date   = "Feb-14"
-greatest_dec_amount = -1825558
+#greatest_inc_date   = "Aug-16"
+#greatest_inc_amount = 1862002
+#greatest_dec_date   = "Feb-14"
+#greatest_dec_amount = -1825558
 
 # output
 
