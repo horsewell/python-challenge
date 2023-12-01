fbd5c57e11f589b487f5e17ba7dd2ebb5f50264b
Tyson <tyson@horsewell.me>
2023-12-01 08:05:59 +0800

    Read CSV file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,38 @@
-# load the data
+# Modules
+import os
+import csv
 
-# store the header
+# Set path for file
+csvpath = os.path.join(".", "Resources", "budget_data.csv")
+
+data = []
+
+# Open the CSV using the UTF-8 encoding
+with open(csvpath, encoding='UTF-8') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # store the header
+    header_row = next(csv_reader)   
+
+    for row in csv_reader:
+        data = row
 
 # analysis
+
+total_months        = 0
+total_amount        = 0
+average_change      = 0
+greatest_inc_date   = ""
+greatest_inc_amount = 0
+greatest_dec_date   = ""
+greatest_dec_amount = 0
+
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in profits (date and amount) over the entire period
+
 
 total_months        = 86
 total_amount        = 22564198
@@ -11,12 +41,6 @@ greatest_inc_date   = "Aug-16"
 greatest_inc_amount = 1862002
 greatest_dec_date   = "Feb-14"
 greatest_dec_amount = -1825558
-
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
 
 # output
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,38 +1,48 @@
-# load the data
+# Modules
+import os
+import csv
 
-# store the header
+# Set path for file
+csvpath = os.path.join(".", "Resources", "election_data.csv")
+
+data = []
+
+# Open the CSV using the UTF-8 encoding
+with open(csvpath, encoding='UTF-8') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # store the header
+    header_row = next(csv_reader)   
+
+    for row in csv_reader:
+        data = row
 
 # analysis
 
-total_months        = 0
-total_amount        = 0
-average_change      = 0
-greatest_inc_date   = ""
-greatest_inc_amount = 0
-greatest_dec_date   = ""
-greatest_dec_amount = 0
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
 
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
-
-
-total_months        = 86
-total_amount        = 22564198
-average_change      = -8311.11
-greatest_inc_date   = "Aug-16"
-greatest_inc_amount = 1862002
-greatest_dec_date   = "Feb-14"
-greatest_dec_amount = -1825558
+total_votes = 369711
+candidates  = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
+count_votes = [85213, 272892, 11606]
+count_max   = 0
+winner      = ""
 
 # output
 
-print(f"Financial Analysis")
-print(f"----------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${total_amount}")
-print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: {greatest_inc_date} (${greatest_inc_amount})")
-print(f"Greatest Decrease in Profits: {greatest_dec_date} (${greatest_dec_amount})")
+print(f"Election Results")
+print(f"-------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"-------------------------")
+for candidate in candidates:
+    candidate_votes = count_votes[candidates.index(candidate)]
+    print(f"{candidate}: {candidate_votes/total_votes:.3%} ({candidate_votes})")
+    if candidate_votes > count_max:
+        count_max = candidate_votes
+winner = candidates[count_votes.index(count_max)]
+print(f"-------------------------")
+print(f"Winner: {winner}")
+print(f"-------------------------")
